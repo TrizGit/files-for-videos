c88f495406b950b1e295ce8f0eaa18a37252c5c8
Triz_Git <78328129+TrizGit@users.noreply.github.com>
2025-09-11 21:53:11 +0700

    visually looks neat, i think
    what do you think?

--- scripts/modules/InfobarModule.hx
+++ scripts/modules/InfobarModule.hx
@@ -1,7 +1,7 @@
 /**
  * Uh oh. I don't think github doesn't recognize .hxc files as Haxe.
  * I'm just gonna rename this to a .hx file temporarily.
- * Remember to change the extension to .hxc.
+ * Remember to change the extension to .hxc, or just download the binary file.
  */
 
 import funkin.Highscore;
@@ -26,9 +26,9 @@ import flixel.util.FlxColor;
  * 
  * ...
  * 
- * Just know that if you're an experienced programmer, try to fix this mess that i made. -- Triz Game (aaaaaaaaaaaaaaaaaaa-)
+ * Just know that if you're an experienced programmer, try to fix this mess that i made. --Triz Game (aaaaaaaaaaaaaaaaaaa-)
  * 
- * (god, i hate having to add ; to almost every line.)
+ * (god, i hate forgetting to add ; to almost every line.)
  */
 
 class InfobarModule extends Module {
@@ -42,94 +42,96 @@ class InfobarModule extends Module {
   var accuracy:Float = 0.0;
   var tallyScore:Int = 0;
   var maxTallyScore:Int = 0;
-  var screenCenter:Float = FlxG.width / 2;
-
+  
+  var screenCenter:Float = FlxG.width / 2; // wait, screen size is always even numbers... oh well.
   var rankInfo = null;
-
+  var okToRun = false; // just checkin' if it's a PlayState and not any other so it doesn't throw an error.
+  
   var last_comboBreaks:Int = 0;
   var last_combo:Int = 0;
-  var last_tnh:Int = 0; // Debugging purposes (but pls don't comment this)
-
+  var last_tnh:Int = 0; // debugging purposes (don't comment it tho.)
+  
   var miss_text:FlxText;
   var acc_text:FlxText;
+  var x_offset:Int = 63; // "what's this?" | check the xChangeByLength [line 203] function, i dare you.
   
-  // Fun fact: I like to put stuff as a new function so i don't get confused.
-  // ...and i think you got confused.
-
+  // Fun fact: I like to put stuff as a new function so i don't get confused... and i think you got confused. Oh well.
+  
   /**
    * Just a function only available on my scripts.
-   * Please keep this. (or even better, use it.)
+   * just leave this be, stop threatening it.
    */
-  function traceTG(text:Str) {
-    trace('TrizGit Scripts >>> ' + text);
-  }
-  
+   function traceTG(text:Str) {
+     trace('TrizGit Scripts >>> ' + text);
+    }
+    
   /**
    * Traces on player hit. Use for debugging.
    */
-  function traceOnPlayerNoteHit(text:Str) {
-    if (Highscore.tallies.totalNotesHit > last_tnh) {
-      traceTG(text);
-      last_tnh = Highscore.tallies.totalNotesHit;
-    }
+   function traceOnPlayerNoteHit(text:Str) {
+     if (Highscore.tallies.totalNotesHit > last_tnh) {
+       traceTG(text);
+       last_tnh = Highscore.tallies.totalNotesHit;
+      }
   }
 
   /**
-   * Colors the last letter of a text. I hate to do this so much.
-   * @param text The text. FlxText.
+   * Colors the last letter of a text.
+   * i hate to do this so much.
+   * @param text The text. FlxText. what else?
    */
   function colorMeSurprise(text:FlxText, color:Int) {
+    if (!okToRun) return;
+
     var textLength = text.text.length;
-    // Is it a bad idea to put it in onUpdate?
-    try {
-      text.clearFormats();
-      text.addFormat(new FlxTextFormat(color), textLength - 1, textLength);
-    } catch (e:Dynamic) {
-      traceTG('An error has occured! ['+e+']');
-    }
+    // me when i put it in onUpdate: error
+    text.clearFormats();
+    text.addFormat(new FlxTextFormat(color), textLength - 1, textLength);
   }
     
   /**
-   * Creates the text. The text creation logic was stolen from the score text lmao.
-   * @param cur_state Checks for the current state. (I have no idea why i added this.)
+   * Creates the text.
+   * the logic "borrowed" from the score text lmao.
+   * @param cur_state checks for the current state. (i dunno why i added this.)
    */
   function createText(cur_state:PlayState) {
     infoBarYPos = (Preferences.downscroll) ? FlxG.height * 0.1 : FlxG.height * 0.9; // this is the math they calculated to place the score text's y pos
-
-    miss_text = new FlxText(screenCenter - 65, infoBarYPos + 30, 0, 'Combo Breaks: 0', 16);
-    miss_text.setFormat(Paths.font('vcr.ttf'), 16, 0xFFFFFF, 'CENTER', FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-    miss_text.scrollFactor.set();
-    miss_text.zIndex = 802;
-    miss_text.cameras = [cur_state.camHUD];
-    cur_state.add(miss_text);
     
     acc_text = new FlxText(screenCenter - 280, infoBarYPos + 30, 0, 'Accuracy: N/A% - ?', 16);
     acc_text.setFormat(Paths.font('vcr.ttf'), 16, 0xFFFFFF, 'CENTER', FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
     acc_text.scrollFactor.set();
     acc_text.zIndex = 802;
     acc_text.cameras = [cur_state.camHUD];
+    acc_text.moves = true;
     cur_state.add(acc_text);
-
+    
+    miss_text = new FlxText(screenCenter - x_offset, infoBarYPos + 30, 0, 'Combo Breaks: 0', 16);
+    miss_text.setFormat(Paths.font('vcr.ttf'), 16, 0xFFFFFF, 'CENTER', FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+    miss_text.scrollFactor.set();
+    miss_text.zIndex = 802;
+    miss_text.cameras = [cur_state.camHUD];
+    cur_state.add(miss_text);
+    
     traceTG('The texts are created!');
   }
 
   /**
    * Returns a respective "accuracy rank" to the text.
-   * There's only PFC and GFC since bads and shits are considered misses.
-   * @param accCheck Checks the current accuracy. (I don't know why, but it's better.)
+   * there's only PFC and GFC since bads and shits are considered misses.
+   * @param accCheck checks the current accuracy. (idk why i added this, but uhhh... keep it.)
    */
   function accInfo(accCheck:Float) {
-    if (accCheck == 1 && Highscore.tallies.good < 1) return 'PFC%';
-    else if (accCheck == 1) return 'GFC%';
-    else return FlxMath.roundDecimal(accCheck * 100, 2) +'%';
+    if (accCheck == 1 && Highscore.tallies.good < 1)  return 'PFC%';
+    else if (accCheck == 1)                           return 'GFC%';
+    else return                                       FlxMath.roundDecimal(accCheck * 100, 2) +'%';
   }
 
   /**
    * Returns a song rank. Rank is actually based on the rank system in v-slice.
-   * @param accCheck Checks the current accuracy. (I don't know why, but it's better.)
+   * @param accCheck checks the current accuracy. (idk why i added this, but uhhh... keep it.)
    */
   function accRank(accCheck:Float):{rank:Str, color:Int} {
-    return switch (true) { // Figured out how to use the switch function :)
+    return switch (true) { // using the switch function like an if else statement... good job me.
       case accCheck == 1.0 && Highscore.tallies.good < 1: // Perfect Gold
         {rank: 'P', color: 0xFFB619};
       case accCheck == 1.0: // Perfect
@@ -142,7 +144,7 @@ class InfobarModule extends Module {
         {rank: 'G', color: 0xEF8764};
       case accCheck < 0.6: // Shit (or Loss)
         {rank: 'L', color: 0x6044FF};
-      default:
+      default: // Only at the start before note hits, or maybe errors happen idk.
         {rank: '?', color: 0xFFFFFF};
     }
   }
@@ -164,8 +166,8 @@ class InfobarModule extends Module {
   }
   
   /**
-   * Calculates the judgements, accuracy, and stuff
-   * This stumped me for a while lol.
+   * Calculates the judgements, accuracy, and stuff idk.
+   * this stumped me for a while lol.
    */
   function calcTallyAndUpdateText() {
     tallyScore = (Highscore.tallies.sick + Highscore.tallies.good - Highscore.tallies.missed);
@@ -173,9 +175,14 @@ class InfobarModule extends Module {
     comboBreaks = Highscore.tallies.bad + Highscore.tallies.shit + hold_misses + Highscore.tallies.missed;
     
     if (maxTallyScore >= 1) {
+      // again, this stumped me for a while. I wonder why... (Hint: this line you're looking at rn.)
       if (Highscore.tallies.combo < last_combo && comboBreaks <= last_comboBreaks) hold_misses += 1;
-      // Again, this stumped me for a while. I wonder why... (Hint: the line you're looking at rn.)
       
+      /**
+       * errors happens to the best of us...
+       * update: i just learnt this thing called "Press F5 to dump the cache without restating the engine." -_-, how tf did i not notice this. 
+       * oh well, i'm keeping it incased in this try function.
+       */
       try {
         rankInfo = accRank(accuracy);
         accuracy = ((tallyScore / maxTallyScore) < 0) ? 0 : (tallyScore / maxTallyScore);
@@ -190,24 +197,41 @@ class InfobarModule extends Module {
     last_combo = Highscore.tallies.combo;
   }
 
-  // From here, it's just override functions. I don't (really) need to explain them.
-  
+  /**
+   * Just visual stuff that you probably won't notice, fuuuuuuuuuu-
+   */
+  function xChangeByLength() {
+    if (!okToRun) return;
+
+    acc_text_math = acc_text.text.length - 17; // oh fuck you, i did what i have to do
+
+    // my math ain't mathing, wtf
+    if (comboBreaks < 10)       miss_text.x = screenCenter - x_offset + (acc_text_math * 8 - acc_text_math * 4) - 6;
+    else if (comboBreaks < 100) miss_text.x = screenCenter - x_offset + (acc_text_math * 6 - acc_text_math * 4) - 10;
+    else                        miss_text.x = screenCenter - x_offset - 12;
+
+    traceOnPlayerNoteHit('\nAcc Text: ' + acc_text.text.length + ' | Miss Text: ' + miss_text.text.length + '\nThe math: ' + miss_text.x);
+  }
+
+  // From here, it's just override functions. I don't (really) need to explain them, soooooooooooooo..... reading past this is basically useless.
   override function onSongLoaded(event:SongLoadScriptEvent) {
     super.onSongLoaded(event);
-    
     var state:PlayState = PlayState.instance;
     createText(state);
     resetText();
+    okToRun = true;
   }
-
+  
   override function onDestroy(event:ScriptEvent) {
     if (miss_text != null) miss_text.destroy();
     if (acc_text != null) acc_text.destroy();
+    okToRun = false;
   }
 
   override function onUpdate(event:ScriptEvent):Void {
     super.onUpdate(event);
     calcTallyAndUpdateText();
+    xChangeByLength();
   }
 
   override function onSongRetry(event:SongRetryEvent) {
